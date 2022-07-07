082a9c4402e931fdbcdec1c07d4f425539aef72a
Diego Yuri <diegoyuri@msn.com>
2022-07-07 01:29:12 -0300

    feat: Categories widget added.

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
 import { Category } from '../types/graphql_queries'
+import { getCategories } from '../services/graphcmsclient'
+
 
 interface CategoriesProps {
   categories: Array<Category>
 }
+
 const Categories = ({}: CategoriesProps) => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories()
+      .then((newCategories) => setCategories(newCategories))
+  }, [])
+
   return (
-    <div>Categories</div>
+    <div className='bg-slate-700 shadow-xl rounded-lg p-8 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        Categorias
+      </h3>
+      {categories.map((category: Category) => (
+        <Link key={category.slug} href = {`/category/${category.slug}`}>
+          <span className="cursor-pointer block pb-3 mb-3">
+            {category.name}
+          </span>
+        </Link>
+
+      ))}
+    </div>
   )
 }
 
