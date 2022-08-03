ee7df36077361ae94a2198f6e829114367238c54
Diego Yuri <diegoyuri@msn.com>
2022-08-03 16:48:18 -0300

    (feat) added detailed post query

--- services/graphcmsclient.ts
+++ services/graphcmsclient.ts
@@ -1,48 +1,85 @@
 import { request, gql } from "graphql-request";
-import { Category, Post } from "../types/graphql_queries";
+import { Category, Postcard } from "../types/graphql_queries";
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_EP 
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_EP;
 
-export const getPosts = async (): Promise<Array<Post>> => { 
-  if (!graphqlAPI) return []
-  const query = gql`
-    query Post {
-      postsConnection {
-        edges {
-          node {
-            author {
-              bio
-              name
-              id
-              photo {
-                url
-              }
+export const getPosts = async (): Promise<Array<Postcard>> => {
+    if (!graphqlAPI) return [];
+    const query = gql`
+        query Post {
+            postsConnection {
+                edges {
+                    node {
+                        author {
+                            bio
+                            name
+                            id
+                            photo {
+                                url
+                            }
+                        }
+                        createdAt
+                        slug
+                        title
+                        excerpt
+                        featuredImage {
+                            url
+                        }
+                        categories {
+                            name
+                            slug
+                        }
+                    }
+                }
             }
-            createdAt
-            slug
-            title
-            excerpt
-            featuredImage {
-              url
-            }
-            categories {
-              name
-              slug
-            }
-          }
         }
-      }
-    }
-  `
+    `;
 
-  const result = await request(graphqlAPI, query)
-  
-  return result.postsConnection.edges
-}
+    const result = await request(graphqlAPI, query);
+
+    return result.postsConnection.edges;
+};
+
+export const getPostDetails = async (slug: String): Promise<Array<Postcard>> => {
+    if (!graphqlAPI) return [];
+    const query = gql`
+        query GetPostDetails($slug: String!) {
+            post(where: { slug: $slug }) { 
+                author {
+                    bio
+                    name
+                    id
+                    photo {
+                        url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                }
+                content { 
+                    raw
+                }
+            }
+        }
+    `;
+    const result = await request(graphqlAPI, query, { slug });
+
+    return result.post;
+};
 
 export const getRecentPosts = async () => {
-  if (!graphqlAPI) { return }
-  const query= gql`
+    if (!graphqlAPI) {
+        return;
+    }
+    const query = gql`
     query GetPostDetails() {
       posts(
         orderBy:createdAt_ASC
@@ -56,16 +93,17 @@ export const getRecentPosts = async () => {
         slug
       }
   }
-  `
-  const result = await request(graphqlAPI, query)
+  `;
+    const result = await request(graphqlAPI, query);
 
-  return result.posts
-}
+    return result.posts;
+};
 
-
-export const getSimilarPosts = async(categories: string, slug: string) => {
-  if (!graphqlAPI) { return }
-  const query = gql`
+export const getSimilarPosts = async (categories: string, slug: string) => {
+    if (!graphqlAPI) {
+        return;
+    }
+    const query = gql`
   query GetPostDetails($slug: String!, $categories: [String!]) {
     posts(
       where: { slug_not: $slug, AND {categories_some: { slug_in: $categories}}}
@@ -78,25 +116,27 @@ export const getSimilarPosts = async(categories: string, slug: string) => {
         createdAt
         slug
       }
-  }`
-  const result = await request(graphqlAPI, query)
+  }`;
+    const result = await request(graphqlAPI, query, { categories, slug });
 
-  return result.posts
-}
+    return result.posts;
+};
 
 export const getCategories = async () => {
-  if (!graphqlAPI) { return }
+    if (!graphqlAPI) {
+        return;
+    }
 
-  const query = gql`
+    const query = gql`
   query GetCategories() {
     categories {
       name
       slug
     }
   }
-  `
+  `;
 
-const result = await request(graphqlAPI, query)
+    const result = await request(graphqlAPI, query);
 
-return result.categories
-}
+    return result.categories;
+};
