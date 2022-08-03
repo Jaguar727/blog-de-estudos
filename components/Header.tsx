85faca356fb74bcae3d59ce45023fe75ed8e8357
Diego Yuri <diegoyuri@msn.com>
2022-08-03 16:50:49 -0300

    (fix) improved indentation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,44 @@
-import Link from 'next/link'
-import React, { useState, useEffect } from 'react'
-import { Category } from '../types/graphql_queries'
-import { getCategories } from '../services/graphcmsclient'
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { Category } from "../types/graphql_queries";
+import { getCategories } from "../services/graphcmsclient";
 
 interface CategoriesProps {
-  categories: Array<Category>
+    categories: Array<Category>;
 }
 
 const Header = () => {
-  const [categories, setCategories] = useState([])
-  
-  useEffect(() => {
-    getCategories()
-      .then((newCategories) => setCategories(newCategories))
-  }, [])
+    const [categories, setCategories] = useState([]);
 
-  return (
-    <div className='container mx-auto px-10 mb-8'>
-      <div className='border-b w-full inline-block border-blue-400 py-8'>
-        <div className='md:float-left block'>
-          <Link href="/">
-            <span className='cursor-pointer font-bold text-4xl'>Jaguar's Studies</span>
-          </Link>
-        </div>
-        <div className='hidden md:float-left md:contents'>
-          {categories.map((category: Category) => (
-            <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className='md:float-right mt-2 align-middle ml-4 font-semibold cursor-pointer'>
-                {category.name}
-              </span>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
-}
+    useEffect(() => {
+        getCategories().then(newCategories => setCategories(newCategories));
+    }, []);
 
-export default Header
+    return (
+        <div className="container mx-auto px-10 mb-8">
+            <div className="border-b w-full inline-block border-blue-400 py-8">
+                <div className="md:float-left block">
+                    <Link href="/">
+                        <span className="cursor-pointer font-bold text-4xl">
+                            Jaguar's Studies
+                        </span>
+                    </Link>
+                </div>
+                <div className="hidden md:float-left md:contents">
+                    {categories.map((category: Category ) => (
+                        <Link
+                            key={category.slug}
+                            href={`/category/${category.slug}`}
+                        >
+                            <span className="md:float-right mt-2 align-middle ml-4 font-semibold cursor-pointer">
+                                {category.name}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
